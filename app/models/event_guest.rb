17e28139e4e0b58977de8eab9423203f40077b5c
Thomas Bender <thomas.bender82@qmail.cuny.edu>
2024-04-07 16:55:45 -0400

    Better....

--- app/models/event_guest.rb
+++ app/models/event_guest.rb
@@ -4,10 +4,10 @@
 class EventGuest < ApplicationRecord
   belongs_to :event
   belongs_to :contact
+  belongs_to :guest, class_name: 'Contact', foreign_key: 'contact_id' # alias
 
   accepts_nested_attributes_for :contact
 
-  alias guest contact
   # Create a barcode service thing going on here
   def assign_barcode
     self.barcode = SecureRandom.hex(8)
